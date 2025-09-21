1d9ec67bb8f7aff1dc34c10ffd618795b10314cc
VYKUNTESWARARAO <chinnimeesala2002@gmail.com>
2025-09-21 16:48:20 +0530

    Fix CI/CD workflow and add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/flat-management-system/", // 👈 add this line (replace with your repo name)
+});
